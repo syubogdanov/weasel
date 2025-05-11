97da4f3ff79b4140249e49cb6ec2a1c1c4687a1e
Sergei Bogdanov <77158975+syubogdanov@users.noreply.github.com>
2025-05-11 20:52:03 +0300

    feat: levenshtein estimator

---
+++ weasel/infrastructure/estimators/levenshtein.py
@@ -0,0 +1,12 @@
+from rapidfuzz.distance.Levenshtein import normalized_similarity
+
+from weasel.domain.services.interfaces.estimator import EstimatorInterface
+
+
+class LevenshteinEstimator(EstimatorInterface):
+    """The Levenshtein estimator."""
+
+    @classmethod
+    def estimate(cls, source: str, target: str) -> str:
+        """Estimate whether `source` is derived from `target`."""
+        return normalized_similarity(source, target)
