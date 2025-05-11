e80d305eb6657c357953f2b034e386686b91ffa8
Sergei Bogdanov <77158975+syubogdanov@users.noreply.github.com>
2025-05-11 22:44:14 +0300

    feat: extend the container

--- weasel/container.py
+++ weasel/container.py
@@ -6,8 +6,8 @@ from dependency_injector.providers import Provider, Singleton
 from weasel.infrastructure.estimators.damerau_levenshtein import DamerauLevenshteinEstimator
 from weasel.infrastructure.estimators.jaro_winkler import JaroWinklerEstimator
 from weasel.infrastructure.estimators.levenshtein import LevenshteinEstimator
-from weasel.infrastructure.mutations.python import py001, py002, py003
-from weasel.infrastructure.mutations.starlark import bzl001, bzl002
+from weasel.infrastructure.mutations.python import py001, py002, py003, py004, py005
+from weasel.infrastructure.mutations.starlark import bzl001, bzl002, bzl003, bzl004
 
 
 if TYPE_CHECKING:
@@ -26,10 +26,14 @@ class WeaselContainer(DeclarativeContainer):
 
     bzl001: Provider["MutationInterface"] = Singleton(bzl001.StarlarkMutation)
     bzl002: Provider["MutationInterface"] = Singleton(bzl002.StarlarkMutation)
+    bzl003: Provider["MutationInterface"] = Singleton(bzl003.StarlarkMutation)
+    bzl004: Provider["MutationInterface"] = Singleton(bzl004.StarlarkMutation)
 
     py001: Provider["MutationInterface"] = Singleton(py001.PythonMutation)
     py002: Provider["MutationInterface"] = Singleton(py002.PythonMutation)
     py003: Provider["MutationInterface"] = Singleton(py003.PythonMutation)
+    py004: Provider["MutationInterface"] = Singleton(py004.PythonMutation)
+    py005: Provider["MutationInterface"] = Singleton(py005.PythonMutation)
 
 
 WEASEL_CONTAINER = WeaselContainer()
