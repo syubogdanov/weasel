5068e1773e9677b2eaff349a901c12f5ad1be028
Sergei Bogdanov <77158975+syubogdanov@users.noreply.github.com>
2025-05-12 00:31:08 +0300

    feat: PY006

--- weasel/infrastructure/mutations/python/py006.py
+++ weasel/infrastructure/mutations/python/py006.py
@@ -3,6 +3,9 @@ import ast
 from dataclasses import dataclass
 from typing import TYPE_CHECKING
 
+from networkx.algorithms.matching import max_weight_matching
+from networkx.classes import Graph
+
 from weasel.domain.services.interfaces.mutation import MutationInterface
 
 
@@ -64,9 +67,41 @@ class PythonReorderer:
         body = self._reorder_list(source.body, target.body)
         return ast.Module(body=body, type_ignores=source.type_ignores)
 
-    def _reorder_list(self, source: list[ast.AST], target: list[ast.AST]) -> list[ast.AST]:  # noqa: ARG002
+    def _reorder_list(self, source: list[ast.AST], target: list[ast.AST]) -> list[ast.AST]:
         """Reorder `source` using `target` as the reference."""
-        return source
+        source_blocks = self._split_into_blocks(source)
+        target_blocks = self._split_into_blocks(target)
+
+        matchings = self._match_blocks(source_blocks, target_blocks)
+
+        blocks: list[ast.AST | list[ast.AST]] = [[]] * len(source_blocks)
+
+        min_index = min(matchings)
+        max_index = max(matchings)
+
+        for index in range(min_index):
+            blocks[index] = source_blocks[index]
+
+        for index in range(max_index + 1, len(source_blocks)):
+            blocks[index] = source_blocks[index]
+
+        reordering = sorted(matchings, key=lambda old_index: matchings[old_index])
+
+        for new_index, old_index in enumerate(reordering, start=min_index):
+            target_index = matchings[old_index]
+
+            source_block = source_blocks[old_index]
+            target_block = target_blocks[target_index]
+
+            if isinstance(source_block, ast.ClassDef) and isinstance(target_block, ast.ClassDef):
+                source_block = self._reorder_class(source_block, target_block)
+
+            blocks[new_index] = source_block
+
+            if new_index != old_index:
+                self._triggered = True
+
+        return self._merge_blocks(blocks)
 
     def _reorder_class(self, source: ast.ClassDef, target: ast.ClassDef) -> ast.ClassDef:
         """Reorder `source` using `target` as the reference."""
@@ -79,10 +114,59 @@ class PythonReorderer:
             type_params=source.type_params,
         )
 
-    def _compare_blocks(
+    def _compare_two_blocks(
         self, source: ast.AST | list[ast.AST], target: ast.AST | list[ast.AST]
     ) -> float:
         """Compare `source` and `target` blocks."""
         if isinstance(source, ast.ClassDef) and isinstance(target, ast.ClassDef):
             source = self._reorder_class(source, target)
         return self._estimator.estimate(ast.unparse(source), ast.unparse(target))
+
+    def _match_blocks(
+        self, source: list[ast.AST | list[ast.AST]], target: list[ast.AST | list[ast.AST]]
+    ) -> dict[int, int]:
+        """Match `source` and `target` blocks."""
+        graph = Graph()
+
+        for index1, block1 in enumerate(source):
+            for index2, block2 in enumerate(target, start=len(source)):
+                weight = self._compare_two_blocks(block1, block2)
+                graph.add_edge(index1, index2, weight=weight)
+
+        return {
+            min(index1, index2): max(index1, index2) - len(source)
+            for (index1, index2) in max_weight_matching(graph)
+        }
+
+    @classmethod
+    def _split_into_blocks(cls, source: list[ast.AST]) -> list[ast.AST | list[ast.AST]]:
+        """Split the list of nodes into blocks."""
+        blocks: list[ast.AST | list[ast.AST]] = []
+        block: list[ast.AST] = []
+
+        for node in source:
+            if not isinstance(node, (ast.AsyncFunctionDef, ast.ClassDef, ast.FunctionDef)):
+                block.append(node)
+                continue
+            if block:
+                blocks.append(block)
+                block = []
+            blocks.append(node)
+
+        if block:
+            blocks.append(block)
+
+        return blocks
+
+    @classmethod
+    def _merge_blocks(cls, blocks: list[ast.AST | list[ast.AST]]) -> list[ast.AST]:
+        """Merge the blocks into a single list."""
+        nodes: list[ast.AST] = []
+
+        for block in blocks:
+            if isinstance(block, list):
+                nodes.extend(block)
+            else:
+                nodes.append(block)
+
+        return nodes
