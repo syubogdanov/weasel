fffe40ae3de1a188b3ea5c410bf2379cbf9e3e4b
Sergei Bogdanov <77158975+syubogdanov@users.noreply.github.com>
2025-05-11 19:14:28 +0300

    feat: readthedocs

---
+++ docs/conf.py
@@ -0,0 +1,12 @@
+author = "Sergei Y. Bogdanov"
+copyright = "2025, Sergei Y. Bogdanov"
+html_theme = "alabaster"
+html_theme_options = {
+    "description": "...",
+    "github_banner": True,
+    "github_button": True,
+    "github_repo": "weasel",
+    "github_type": "star",
+    "github_user": "syubogdanov",
+}
+project = "weasel"
